06e56216caba046560e8dfe6a4841d56b64f58f6
H Joseph <hjoseph777@gmail.com>
2024-10-11 21:05:37 -0400

    main

---
+++ script.js
@@ -0,0 +1,39 @@
+function greet(name) {
+    return "Hello " + name;
+}
+console.log(greet("World"));
+
+
+function addNumbers(num1, num2) {
+    let sum = num1 + num2;
+    return sum;
+}
+
+console.log(addNumbers(10, 20));
+
+
+let num = 10;
+
+function showNum() {
+  let num = 30;
+  console.log(num);
+}
+
+console.log(num);
+showNum();
+console.log(num);
+
+function makeCounter() {
+  let count = 0;
+
+  return function() {
+    count++;
+    console.log("Count is now:", count);
+  }
+}
+
+let counter = makeCounter();
+
+counter();
+counter();
+counter();
